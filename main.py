38f0ded25f5d0b82c6e0fbfcd08fc49a6d277634
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-24 21:52:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
 
-    loop = asyncio.new_event_loop()
+    loop = syncio.new_event_loop()
     asyncio.set_event_loop(loop)
     
     loop.run_until_complete(run_bot())
