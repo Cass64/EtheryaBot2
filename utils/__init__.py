e34d01af84736c16ea55dd91733788b6a50a6f4e
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-26 20:53:38 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 # FILE: /EtheryaBot/EtheryaBot/utils/__init__.py
-from .database import load_data, save_data
+from .database import get_config_data as load_data, save_config_data as save_data
 from .keep_alive import keep_alive
